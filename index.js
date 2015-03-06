169710dc63fb19b0c34f0373bec3c39bd4ca322f
Idan Gazit <idan@gazit.me>
2015-03-06 19:37:20 +0200

    Correctly set due date for cards to allballs:today +44 hrs

--- index.js
+++ index.js
@@ -30,7 +30,7 @@
 
     var trelloParams = {
       pos: 'top',
-      due: moment(today).add(20, 'hours'),
+      due: moment(today).add(44, 'hours'),
       idList: DESTINATION_LIST,
       idMembers: MEMBER_IDS,
       idCardSource: cardId
